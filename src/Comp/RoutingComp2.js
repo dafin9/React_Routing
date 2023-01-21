f92f1feb3abc4444ab900fa9a9d08755e9876fe6
Abhi...!! <Abhi.d3v3lop3r@gmail.com>
2023-01-21 18:52:37 +0530

    Update

--- src/Comp/RoutingComp2.js
+++ src/Comp/RoutingComp2.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import logo from '../logo.svg';
 import { Link } from 'react-router-dom';
 
 const RoutingComp2 = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
